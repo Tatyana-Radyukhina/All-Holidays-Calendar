e01e4d97559ea45d747ba59223da4b039b1b9347
Tatyana-Radyukhina <t.a.radyukhina@yandex.ru>
2022-12-15 22:47:05 +0300

    Add layouts and mock resourses in application

---
+++ app/src/main/java/com/example/allholidayscalendar/ResultFragment.kt
@@ -0,0 +1,23 @@
+package com.example.allholidayscalendar
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.allholidayscalendar.databinding.FragmentResultBinding
+
+
+class ResultFragment : Fragment() {
+
+    private lateinit var binding: FragmentResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
